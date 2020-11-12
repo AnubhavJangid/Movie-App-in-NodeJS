b5d7723b56ded74dbd4ac37dda9f8f0e0e4acb84
AnubhavJangid <anubhavjangid007@gmail.com>
2020-11-12 13:13:36 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const { error } = require('console')
+const express = require('express')
+const app = express()
+const path = require('path')
+const request = require('request')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.listen(3000, () => {
+    console.log('Server running at port 3000')
+})
+
+app.get('/search', (req, res) => {
+    res.render('search')
+})
+
+app.get('/movie', (req, res) => {
+    res.render('movie')
+})
+
+app.get('/results', (req, res) => {
+
+    let query = req.query.search
+
+    request('https://api.themoviedb.org/3/search/movie?api_key=89af04b9e59028670c97874bb0ca4f5e&query='+ query, (error, response, body) => {
+        if(error)
+        {
+            console.log(error)
+        }
+        
+        let data = JSON.parse(body)
+        res.render('movie', {data:data , searchQuery: query})
+    })
+})
